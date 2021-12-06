52ca074ab3839cc4ae42d3d0357bfa725bcfb7f0
GiovanniMarcinari <gmarcinari@gmail.com>
2021-12-06 02:04:41 -0300

    Estilizando o Blog com CSS

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function home () {
+    return (
+        <>
+        <h1 className="titulo">Home</h1>
+        <img src="C:\Users\tania\Desktop\React\blog-pessoal\ReactBlogPessoal\assets\EscoladeAtenas" alt="Imagem tela inicial" className="img"/>
+        </>
+    );
+}
+
+export default home;
